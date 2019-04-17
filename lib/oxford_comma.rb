d4e195fd9bb4c45d2cbf566542597b9e0d908639
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:03:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
     return  array.join
     elsif array.size == 2
     return  "#{array[0, array.length - 1].join(", ")} and #{array.last}"
+  elsif array.size == 3
+    return "#{array[0, array.length - 1].join(", ")}, and #{array.last}"
   else
     return "#{array[0, array.length - 1].join(", ")} and #{array.last}"
     end
