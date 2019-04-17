27da246960a8199904e459a5341760195543ea04
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 04:01:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-    array.size < 2? array.join : "#{array[0, array.length - 1].join(", ")} and #{array.last}"
+    if array.size == 1
+    return  array.join
+    elsif array.size == 2
+    return  "#{array[0, array.length - 1].join(", ")} and #{array.last}"
+  elsif array.size == 3
+    return "#{array[0, array.length - 1].join(", ")}, and #{array.last}"
+    end
+    "#{array[0, array.length - 1].join(", ")} and #{array.last}"
 end
